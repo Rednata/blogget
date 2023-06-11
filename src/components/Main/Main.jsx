be4e38c9a9939e0831c9249f6f15ad552d6b0b70
Rednata <maknatosh@gmail.com>
2023-06-11 08:18:49 +0700

    9.2 add Header-components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../Layout';
+import style from './Main.module.css';
+
+export const Main = () => {
+  return (
+    <main className={style.main}>
+      <Layout>
+        
+      </Layout>
+    </main>
+  )
+}
