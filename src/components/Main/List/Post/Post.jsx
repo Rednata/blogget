9ded1596ed69688e045f4a4f00ae795392b4121b
Rednata <maknatosh@gmail.com>
2023-06-17 11:07:17 +0700

    9.4 split Post on components (Content, Rating, Time)

--- src/components/Main/List/Post/Post.jsx
+++ src/components/Main/List/Post/Post.jsx
@@ -1,27 +1,18 @@
 import style from './Post.module.css';
 import PropTypes from 'prop-types';
 import notphoto from './img/notphoto.jpg';
-import formatDate from '../../../../utils/formatDate';
+import Rating from './Rating';
+import Time from './Time';
+import Content from './Content';
 
 export const Post = ({postData}) => {
   const {title, author, ups, date} = postData;
   return (
     <li className={style.post}>
       <img className={style.img} src={notphoto} alt={title} />
-      <div className={style.content}>
-        <h2 className={style.title}>
-          <a className={style.linkPost} href="#post">
-            {title}
-          </a>
-          <a href="#author" className={style.linkAuthor}>{author}</a>
-        </h2>
-      </div>
-      <div className={style.rating}>
-        <button className={style.up} aria-label='Повысить рейтинг' />
-        <p className='style.ups'>{ups}</p>
-        <button className={style.down} aria-label='Понизить рейтинг' />
-      </div>
-      <time className={style.date} dateTime={date}>{formatDate(date)}</time>
+      <Content author={author} title={title} />
+      <Rating ups={ups}/>
+      <Time date={date} />
     </li>);
 };
 
